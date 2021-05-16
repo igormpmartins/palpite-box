b8bfba01cec1a23a7e3c71c523b328cc96b6da6f
igormpmartins <73676465+igormpmartins@users.noreply.github.com>
2021-05-16 20:31:04 -0300

    add geração do cupom

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,8 +1,16 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
+import moment from 'moment'
 import credentials from './credentials.json'
+
 
 const idDoc = '1GFk3gXl7dO74M8KlWolyTQwy8bjtbNPPDxvDRzqk2Uk'
 const doc = new GoogleSpreadsheet(idDoc)
+
+const genCupom = () => {
+    const cupom = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
+    return cupom + '||' + cupom.substr(0, 4) + '-' + cupom.substr(4, 4) + '-' + cupom.substr(8, 4)
+
+}
 
 export default async(req, res) => {
 
@@ -14,12 +22,29 @@ export default async(req, res) => {
         const sheet = doc.sheetsByIndex[1]
         const data = JSON.parse(req.body)
 
+        const sheetCfg = doc.sheetsByIndex[2]
+        await sheetCfg.loadCells('A3:B3')
+
+        const possuiPromo = sheetCfg.getCell(2, 0);
+        const textoPromo = sheetCfg.getCell(2, 1);        
+        const bolPossuiPromo = possuiPromo.value === 'VERDADEIRO'
+
+        let Cupom = ''
+        let Promo = ''
+
+        if (bolPossuiPromo) {
+            Cupom = genCupom(),
+            Promo = textoPromo.value
+        }
+
         await sheet.addRow({
             Nome: data.Nome,
             Email: data.Email,
             Whatsapp: data.Whatsapp,
-            Cupom: data.Cupom,
-            Promo: data.Promo
+            Nota: 5,
+            Cupom,
+            Promo,
+            Data: moment().format('DD/MM/YYYY HH:mm:ss')
             }
         )        
         //console.log(data)
